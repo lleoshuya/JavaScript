f46c831613ace8f7a887724255e6f9511175f818
Leonardo Henrique Rangon Paulino <lleoshuya@outlook.com>
2021-02-21 19:19:06 -0300

    Finalizando o módulo Programação para internet com JavaScript e o Desafio com códigos em JavaScript

---
+++ js/main.js
@@ -0,0 +1,155 @@
+/*var nome = "Leonardo Henrique Rangon Paulino";
+var n1 = 25;
+var n2 = 10;
+var frase = "Blue-Eyes é o Melhor Deck!"
+
+
+
+console.log(nome);
+console.log(n1 * n2);
+//console.log(frase.replace("Blue-Eyes", "Lunalight"));
+console.log(frase.toUpperCase(0));
+
+
+
+//alert(idade + idade2);
+//alert("Bem Vindo " + nome + " que possui " + idade + " anos.");*/
+
+
+/*
+var lista = ["Cat Dancer", "Phanter Dancer", "Leo Dancer"];
+
+lista.push("Sabre Dancer"); // Incluir elemento na lista
+
+//lista.pop(); // Remove ultimo elemento da lista
+
+console.log(lista);
+
+console.log(lista.toString()); // Converter textos da Lista em String
+
+console.log(lista.join(" - ")); //Mudar a separação do texto da lista, já convertendo para String*/
+
+
+/*var carta = { nome: "Cat Dancer", tipo: "Fusão" }
+console.log(carta);
+alert(carta.tipo);*/
+/*
+
+Dicionário
+
+var cartas = [
+    { Nome: "Cat Dancer", Tipo: "Fusão" },
+    { Nome: "Stardust Dragon", Tipo: "Synchro" }
+]
+console.log(cartas);*/
+
+/*
+IF e Prompt na tela
+
+Prompt = Exibir uma caixa de texto na tela, para inserir um valor e 
+guardar numa variável por exemplo
+
+var idade = prompt("Qual sua idade?");
+
+
+if (idade >= 18) {
+    alert("Maior de Idade.");
+} else {
+    alert("Menor de Idade.");
+}*/
+
+/*Laços
+
+var count = 0;
+
+while (count <= 5) {
+    console.log(count);
+    count++;
+};
+
+var count;
+for (count = 0; count <= 10; count++) {
+    console.log("Contando: " + count);
+};*/
+
+/*Datas
+
+var d = new Date();
+console.log(d);18
+console.log("Hora Atual: " + d.getHours() + ":" + d.getMinutes());
+console.log("Data Atual: " + d.getDate() + "/" + d.getMonth() + 1 + "/" + d.getFullYear());*/
+
+/*Funções */
+
+/*function soma(n1, n2) {
+    return n1 + n2;
+};
+console.log(soma(5, 4));*/
+
+/*
+
+function validaIdade(idade) {
+    var validar;
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false;
+    }
+    return validar;
+};
+
+var idade = prompt("Qual sua idade?");
+
+console.log(validaIdade(idade));*/
+
+/*function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome);
+};*/
+
+
+//console.log(setReplace("Vai Japão", "Japão", "Brasil"));
+/*
+function botao() {
+    //alert("Obrigado por clicar!");
+    //document.getElementById("agradecimento");
+    //console.log(document.getElementById("agradecimento"));
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!";
+};
+
+function redir() {
+    window.open("http://lleoshuya.xyz/");
+    //window.location.href = "http://lleoshuya.com.br/";
+};
+
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse!";
+    elemento.innerHTML = "Obrigado por passar o mouse!";
+};
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui.";
+    elemento.innerHTML = "Passe o mouse aqui.";
+};
+
+function load() {
+    alert("Página carregada!");
+};
+
+function change(elemento) {
+    console.log(elemento.value);
+};*/
+/*
+var texto = gets();
+var line = texto.split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B; // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);*/
+
+/*
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+let salary = parseFloat(valor2 * valor3).toFixed(2); // Digite aqui o calculo do salário
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);*/
